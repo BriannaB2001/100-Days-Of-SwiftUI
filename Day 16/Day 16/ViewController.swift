76f54df773265b8d2bc4dbc05cb127cdf4cf9932
Brianna <nanitooze@gmail.com>
2020-09-23 08:59:11 -0600

    starting out

---
+++ Day 16/Day 16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day 16
+//
+//  Created by Brianna Babcock on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day 16/Day 16Tests/Day_16Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Day_16Tests.swift
+//  Day 16Tests
+//
+//  Created by Brianna Babcock on 9/23/20.
+//
+
+import XCTest
+@testable import Day_16
+
+class Day_16Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
