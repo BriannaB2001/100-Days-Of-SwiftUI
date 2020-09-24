924fc746456ad891e9594c093d877ea427248298
Brianna <nanitooze@gmail.com>
2020-09-24 08:30:07 -0600

    Day 19 Success

--- Day 19/Day 19/ContentView.swift
+++ Day 19/Day 19/ContentView.swift
@@ -9,16 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var inputUnit = ""
+    @State private var inputUnit = 0
     @State private var inputNumber = ""
-    @State private var outputUnit = ""
-    let inputUnits = ["Farenheight", "Celcius"]
+    @State private var outputUnit = 0
+    
+    let inputUnits = ["Farenheight", "Celcius", "Kelvin"]
+    let outputUnits = ["Farenheight", "Celcius", "Kelvin"]
+    
+    var tempToTemp: Double {
+        let unitToConvert = Double(inputNumber) ?? 0
+        
+        switch (inputUnit, outputUnit) {
+        case (0,1):
+            // f - c
+            return (unitToConvert - 32) * 1.8
+        case (0,2):
+            // f - k
+            return (unitToConvert - 32) * 1.8 + 273.15
+        
+        case (1,0):
+            // c - f
+            return unitToConvert * 1.8 + 32
+            
+        case (1,2):
+            // c - k
+            return unitToConvert + 273.15
+            
+        case (2,1):
+            // k - c
+            return unitToConvert - 273.15
+        case (2,0):
+            // k - f
+            return (unitToConvert - 273.15) * 1.8 + 32
+            
+        default:
+            return unitToConvert
+        }
+        
+    }
     
     var body: some View {
         
             Form {
-                Section(header: Text("What Unit would You like To Start With")) {
-                    TextField("Degree", text: $inputNumber)
+                Section(header: Text("What Unit Are You Starting With")) {
+                    TextField("Enter Degree", text: $inputNumber)
                     
                     Picker("Temperature Unit", selection: $inputUnit) {
                         ForEach(0 ..< inputUnits.count) {
@@ -28,9 +62,20 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("Result")) {
-                    Text("\(outputUnit)°")
+                Section(header: Text("What Unit Would You Like To Convert To")) {
+                    
+                    Picker("Temperature Unit", selection: $outputUnit) {
+                        ForEach(0 ..< outputUnits.count) {
+                            Text("\(self.outputUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
+                
+                Section(header: Text("Result")) {
+                    Text("\(tempToTemp, specifier: "%.2f")°")
+                }
+               
                 
             }
         }
