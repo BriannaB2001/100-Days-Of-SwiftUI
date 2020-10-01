18c7b0f48feb797ba34058f2ae5361b2b0583246
Brianna <nanitooze@gmail.com>
2020-10-01 09:35:16 -0600

    day 24 challenge 3 partly working

--- Day 21/Day 21/ContentView.swift
+++ Day 21/Day 21/ContentView.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+struct RenderView: View {
+    
+    var name: String
+    
+    var renderView: some View {
+        Image(name)
+        .renderingMode(.original)
+                    .clipShape(Capsule())
+                    .overlay(Capsule().stroke(Color.black, lineWidth: 2))
+                    .shadow(color: .black, radius: 2)
+    }
+}
+
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -34,11 +47,7 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                        RenderView(name: self.countries[number])
                     }
                 }
 
